1f49a1aae3c5b5db73a8e591509f19871a20778c
Lucas Piazzi <40416044+Piazzi@users.noreply.github.com>
2021-09-01 17:41:08 -0300

    Add close mic/camera

--- server.js
+++ server.js
@@ -2,45 +2,48 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
-// setup the view
-app.set('view engine', 'ejs');
-
-// setup the public folder 
-app.use(express.static('public'));
-
 //UUID is a javascript library that allows us to create unique Ids
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-const io = require('socket.io')(server);
-
-const { ExpressPeerServer } = require('peer');
+// setup the view
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-debug: true,
+  debug: true,
 });
 
 // import peer js and public folders
-app.use('/peerjs', peerServer);
-app.use(express.static('public'));
+app.use("/peerjs", peerServer);
 
-// get route
-app.get('/', (req, res) => {
-    // redirect the user to a unique room.
-    res.redirect(`/${uuidv4()}`);
+// setup the public folder 
+app.use(express.static("public"));
+
+// redirect the user to a unique room.
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
 });
 
-
-app.get('/:room', (req, res) => {
+// get route
+app.get("/:room", (req, res) => {
     // renders the ejs file (view)
     //(Embedded JavaScript). EJS is a templating language.
-    res.render('room', { roomId: req.param.room });
+  res.render("room", { roomId: req.params.room });
 });
 
 // listen to a join room event
-io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, userId) => {
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
     });
+  });
 });
 
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
